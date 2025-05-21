6158610afb8060949f3425c922b9b0f1ff89e2e0
Aakrish Lama <al226na@student.lnu.se>
2025-05-21 11:55:15 +0200

    chat container for the messages and begin of react hooks

--- frontend/src/Components/ChatPage.jsx
+++ frontend/src/Components/ChatPage.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
 
 export default function ChatPage() {
+    const [messages, setMessages] = useState([])
+    const [input, setInput] = useState("")
+    const inputRef = useRef(null)
+    const chatBoxRef = useRef(null)
+    const [stompClient, setStompClient] = useState(null)
+    const [roomid, setRoomid] = useState("")
+    
   return (
     <div className=''>
         <header className='dark:border-blue-500 shadow py-5 flex justify-around p-4 items-center dark:bg-blue-800'>
@@ -19,10 +27,20 @@ export default function ChatPage() {
             </div>
         </header>
 
+        {/* messages */}
+        <main className=" border h-screen overflow-auto w-2/3 dark:bg-gray-800 mx-auto my-20 mt-0">
+            <div>
+
+            </div>
+
+        </main>
+
         {/* input message */}
         <div className= " fixed bottom-2 w-2/3 w-full h-16">
             <div className= "h-full border w-2/3 mx-auto dark:bg-grey-800 gap-4 rounded">
-                <input type= "textArea" placeholder= "Type your message here" className="dark:border-gray-700 h-full w-11/12  text-center"></input>
+                <input type= "textArea" placeholder= "Type your message here" 
+                    className="dark:border-gray-700 h-full w-11/12 rounded text-center focus:outline-0">
+                    </input>
                 <button className='btn btn-primary flex rounded-full p-2 mx-2'>send</button>
             </div>
 
